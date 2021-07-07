7066ad523e35af7bc8854d0526e06c3f6cc53833
Jarek <jareksedy@icloud.com>
2021-07-07 20:59:47 +0600

    Initial Commit

---
+++ ys-client-server-1347/homework-1/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  ys-client-server-1347
+//
+//  Created by Ярослав on 07.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
