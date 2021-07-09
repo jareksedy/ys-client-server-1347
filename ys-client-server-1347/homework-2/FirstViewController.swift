cea3fcec2140c2241b4ab524f78fabc90818800d
Jarek <jareksedy@icloud.com>
2021-07-09 20:19:08 +0600

    hw2 preparations.

---
+++ ys-client-server-1347/homework-2/FirstViewController.swift
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.swift
+//  ys-client-server-1347
+//
+//  Created by Ярослав on 09.07.2021.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ ys-client-server-1347/homework-2/SecondViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.swift
+//  ys-client-server-1347
+//
+//  Created by Ярослав on 09.07.2021.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
