cd39a2186990eddefc1ab12fcd9e21e2b49fa355
Jarek <jareksedy@icloud.com>
2021-07-10 01:48:59 +0600

    done most of hw2.

--- ys-client-server-1347/homework-2/SecondViewController.swift
+++ ys-client-server-1347/homework-2/SecondViewController.swift
@@ -8,11 +8,39 @@
 import UIKit
 
 class SecondViewController: UIViewController {
-
+    
+    let vkService = VKService(session: Session.instance)
+    
+    let errorMessage = "Ошибка! Что-то пошло не так!"
+    
+    let searchQuery = "падонки"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        vkService.getUserInfo() { json in
+            print("=== Информация обо мне ===")
+            print(json ?? self.errorMessage)
+        }
         
-        let session = Session.instance
+        vkService.getFriendsList() { json in
+            print("=== Список друзей ===")
+            print(json ?? self.errorMessage)
+        }
         
+        vkService.getPhotos() { json in
+            print("=== Мои фотки ===")
+            print(json ?? self.errorMessage)
+        }
+        
+        vkService.getGroups() { json in
+            print("=== Мои группы ===")
+            print(json ?? self.errorMessage)
+        }
+        
+        vkService.searchGroups(searchQuery: searchQuery, count: 1) { json in
+            print("=== Поиск групп по запросу «\(self.searchQuery)» ===")
+            print(json ?? self.errorMessage)
+        }
     }
 }
