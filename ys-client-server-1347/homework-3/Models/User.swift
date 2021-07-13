ef0ef02c592baad436647b35c1a461f12e549bb1
Jarek <jareksedy@icloud.com>
2021-07-13 14:34:44 +0600

    added user model.

---
+++ ys-client-server-1347/homework-3/Models/User.swift
@@ -0,0 +1,17 @@
+//
+//  User.swift
+//  ys-client-server-1347
+//
+//  Created by Ярослав on 13.07.2021.
+//
+
+import Foundation
+
+struct User {
+    
+    var id: Int
+    var firstName: String
+    var lastName: String
+    var imageURL: String
+
+}
