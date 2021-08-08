9062defe7b064f4cf177c235b5ea934097205a81
Jarek <jareksedy@icloud.com>
2021-08-08 09:40:44 +0600

    displaying post author (user or group) and post date.

--- ys-client-server-1347/homework/Controllers/Feed/FeedTableViewCell.swift
+++ ys-client-server-1347/homework/Controllers/Feed/FeedTableViewCell.swift
@@ -6,14 +6,35 @@
 //
 
 import UIKit
+import Alamofire
 
 class FeedTableViewCell: UITableViewCell {
-
+    @IBOutlet weak var postUserGroupImage: RoundedImageView!
+    @IBOutlet weak var postUserGroupName: UILabel!
+    @IBOutlet weak var postDate: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
+    }
+    
+    func configure(item: Item, profile: Profile? = nil, group: Group? = nil) {
+        
+        if let group = group {
+            postUserGroupName.text = group.name
+            
+            AF.request(group.photo100, method: .get).responseImage { response in
+                guard let image = response.value else { return }
+                self.postUserGroupImage.image = image
+            }
+
+        } else {
+            postUserGroupName.text = "\(profile?.firstName ?? "") \(profile?.lastName ?? "")"
+        }
+        
+        postDate.text = String(item.date)
     }
 }
--- ys-client-server-1347/homework/Controllers/Feed/FeedTableViewController.swift
+++ ys-client-server-1347/homework/Controllers/Feed/FeedTableViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 class FeedTableViewController: UITableViewController {
     
     var feedItems: [Item] = []
+    var feedProfiles: [Profile] = []
+    var feedGroups: [Group] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,6 +19,8 @@ class FeedTableViewController: UITableViewController {
         FeedAPI(Session.instance).get{ [weak self] feed in
             guard let self = self else { return }
             self.feedItems = feed!.response.items
+            self.feedProfiles = feed!.response.profiles
+            self.feedGroups = feed!.response.groups
             self.tableView.reloadData()
         }
     }
@@ -29,7 +33,23 @@ class FeedTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "feedCell", for: indexPath) as! FeedTableViewCell
-        cell.textLabel?.text = feedItems[indexPath.row].text
+        //cell.textLabel?.text = feedItems[indexPath.row].text
+        
+        let currentFeedItem = feedItems[indexPath.row]
+        
+        switch feedItems[indexPath.row].sourceID.signum() {
+        
+        case 1: // Пост пользователя
+            let currentFeedItemProfile = feedProfiles.filter{ $0.id == currentFeedItem.sourceID }[0]
+            cell.configure(item: currentFeedItem, profile: currentFeedItemProfile)
+            
+        case -1: // Пост группы
+            let currentFeedItemGroup = feedGroups.filter{ $0.id == abs(currentFeedItem.sourceID) }[0]
+            cell.configure(item: currentFeedItem, group: currentFeedItemGroup)
+            
+        default: break
+        }
+        
         return cell
     }
 }
