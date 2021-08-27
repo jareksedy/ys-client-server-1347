b96a57fb8f2bf9ee976f63b00a20494c07c6f75c
Jarek <jareksedy@icloud.com>
2021-08-27 21:25:43 +0600

    friends tab completed.

--- ys-client-server-1347/homework/Controllers/Friends/FriendsTableViewController.swift
+++ ys-client-server-1347/homework/Controllers/Friends/FriendsTableViewController.swift
@@ -8,14 +8,30 @@
 import UIKit
 
 class FriendsTableViewController: UITableViewController {
+    
+    var friendItems: [FriendItem] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        FriendAPI(Session.instance).get{ [weak self] friends in
+            guard let self = self else { return }
+            self.friendItems = friends!.response.items
+            self.tableView.reloadData()
+        }
     }
 
     // MARK: - Table view data source
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return friendItems.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "FriendCell") as? FriendsTableViewCell
+        else { return UITableViewCell() }
+        
+        cell.configure(friendItems[indexPath.row])
+        return cell
     }
 }
---
+++ ys-client-server-1347/homework/Models/Friend.swift
@@ -0,0 +1,46 @@
+//
+//  Friend.swift
+//  ys-client-server-1347
+//
+//  Created by Ярослав on 27.08.2021.
+//
+
+import Foundation
+
+// MARK: - Friend
+struct Friends: Codable {
+    let response: FriendResponse
+}
+
+// MARK: - Response
+struct FriendResponse: Codable {
+    let count: Int
+    let items: [FriendItem]
+}
+
+// MARK: - Item
+struct FriendItem: Codable {
+    let firstName: String
+    let id: Int
+    let lastName: String
+    let photo100: String?
+    let online: Int?
+    let lastSeen: LastSeen?
+    let trackCode: String?
+
+    enum CodingKeys: String, CodingKey {
+        case firstName = "first_name"
+        case id
+        case lastName = "last_name"
+        case photo100 = "photo_100"
+        case online
+        case lastSeen = "last_seen"
+        case trackCode = "track_code"
+    }
+}
+
+// MARK: - LastSeen
+struct LastSeen: Codable {
+    let platform: Int?
+    let time: TimeInterval
+}
