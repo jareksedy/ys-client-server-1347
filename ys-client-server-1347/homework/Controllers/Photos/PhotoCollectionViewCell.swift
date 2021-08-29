0913ea6945165110ed4511634e78f3c76337dd89
Jarek <jareksedy@icloud.com>
2021-08-29 14:19:03 +0600

    added transparent image as a placeholder + activity indicator.

--- ys-client-server-1347/homework/Controllers/Photos/PhotoCollectionViewCell.swift
+++ ys-client-server-1347/homework/Controllers/Photos/PhotoCollectionViewCell.swift
@@ -9,4 +9,5 @@ import UIKit
 
 class PhotoCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var photoView: UIImageView!
+    @IBOutlet weak var photoIndicator: UIActivityIndicatorView!
 }
--- ys-client-server-1347/homework/Controllers/Photos/PhotoCollectionViewController.swift
+++ ys-client-server-1347/homework/Controllers/Photos/PhotoCollectionViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 import Alamofire
 import AlamofireImage
 import ImageViewer_swift
+import SDWebImage
 
 class PhotoCollectionViewController: UICollectionViewController {
     
@@ -35,12 +36,13 @@ class PhotoCollectionViewController: UICollectionViewController {
         cell.photoView.image = UIImage(named: "placeholder")
 
         AF.request(photoItems[indexPath.row].photo604!, method: .get).responseImage { response in
-            
+
             guard let image = response.value else { return }
             cell.photoView.image = image
-            
+
             cell.photoView.setupImageViewer(options: [.closeIcon(UIImage(systemName: "arrow.backward")!),
                                                       .theme(self.traitCollection.userInterfaceStyle == .light ? .light : .dark)])
+            cell.photoIndicator.stopAnimating()
         }
         
         return cell
