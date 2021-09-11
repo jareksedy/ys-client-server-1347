d23d2543b718de56341464ce838ec087426d500f
Jarek <jareksedy@icloud.com>
2021-09-11 13:24:13 +0600

    added readmore link

--- ys-client-server-1347/homework/Controllers/Feed/FeedItemTextTableViewCell.swift
+++ ys-client-server-1347/homework/Controllers/Feed/FeedItemTextTableViewCell.swift
@@ -26,21 +26,36 @@ class FeedItemTextTableViewCell: UITableViewCell {
 
         feedItemText.customize { label in
             
-            label.text = text
+            if text.byWords.count > maxWordsCount {
+                
+                label.text = String(describing: text.firstLine!)
+                label.text! += "\n" + readMore
+                
+            } else {
+                label.text = text
+            }
             
             let vkHashTag = ActiveType.custom(pattern: #"#\S+"#)
+            let readMoreType = ActiveType.custom(pattern: "\\s\(readMore)\\b")
             
             label.urlMaximumLength = 22
-            label.enabledTypes = [.url, vkHashTag]
+            label.enabledTypes = [.url, vkHashTag, readMoreType]
             
             label.customColor[vkHashTag] = activeVkHashTagColor
             label.customSelectedColor[vkHashTag] = activeVkHashTagColorSelected
+            
+            label.customColor[readMoreType] = activeURLColor
+            label.customSelectedColor[readMoreType] = activeURLColorSelected
             
             label.URLColor = activeURLColor
             label.URLSelectedColor = activeURLColorSelected
             
             label.handleURLTap { url in
                 UIApplication.shared.open(url)
+            }
+            
+            label.handleCustomTap(for: readMoreType) { _ in
+                label.text = text
             }
         }
     }
